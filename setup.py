2e09e0d942ff07a722e102b929c782af0b8f93e7
bolichep <bolichep@gmail.com>
2015-06-02 20:26:08 -0300

    Corrección en setup.py para empaquetar

--- setup.py
+++ setup.py
@@ -10,5 +10,6 @@ setup(name = "huayra-about",
 #      scripts = ["huayra-about"],
       data_files = [ ("share/huayra-about", ["src/huayra-about.py"]),
                      ("share/huayra-about/media", ["src/media/huayra-menu-huayra.svg","src/media/huayra-about-background.svg"]),
+                     ("share/huayra-about/plugins", ["src/plugins/__init__.py","src/plugins/arch.py"]),
                      ("share/applications", ["huayra-about.desktop"]),
                      ("share/man/man1", ["huayra-about.1"]) ] )
