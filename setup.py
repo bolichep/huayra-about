484d4dbd3a819386213deb97a85c75390e2cf17b
bolichep <bolichep@gmail.com>
2015-11-05 23:55:14 -0300

    dummy version, updated setup.py

--- setup.py
+++ setup.py
@@ -8,8 +8,10 @@ setup(name = "huayra-about",
       url = "https://github.com/bolichep/huayra-about",
 #      py_modules = ["huayra-about"],
 #      scripts = ["huayra-about"],
-      data_files = [ ("share/huayra-about", ["src/huayra-about.py"]),
+      data_files = [ ("share/huayra-about", ["src/huayra-about.py","src/rasti.py","src/markup.py","src/info_table.py"]),
                      ("share/huayra-about/media", ["src/media/huayra-menu-huayra.svg","src/media/huayra-about-background.svg"]),
-                     ("share/huayra-about/plugins", ["src/plugins/__init__.py","src/plugins/arch.py"]),
+                     ("share/huayra-about/plugins", ["src/plugins/__init__.py","src/plugins/arch.py","src/plugins/versiones.py","src/plugins/04link.py","src/plugins/cpu_mem.py"]),
                      ("share/applications", ["huayra-about.desktop"]),
                      ("share/man/man1", ["huayra-about.1"]) ] )
+
+
