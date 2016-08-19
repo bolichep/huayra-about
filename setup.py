dad9ba8ae86ec23f6cd1b437112f46c556fb4c59
bolichep <bolichep@gmail.com>
2016-08-19 02:14:38 -0300

    fix setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(name = "huayra-about",
 #      scripts = ["huayra-about"],
       data_files = [ ("share/huayra-about", ["src/huayra-about.py","src/rasti.py","src/markup.py","src/info_table.py"]),
                      ("share/huayra-about/media", ["src/media/huayra-menu-huayra.svg","src/media/huayra-about-background.svg"]),
-                     ("share/huayra-about/plugins", ["src/plugins/__init__.py","src/plugins/arch.py","src/plugins/versiones.py","src/plugins/04link.py","src/plugins/cpu_mem.py"]),
+                     ("share/huayra-about/plugins", ["src/plugins/__init__.py","src/plugins/01versiones.py","src/plugins/02arch.py","src/plugins/03cpu_mem.py","src/plugins/04kernel.py","src/plugins/05link.py"]),
                      ("share/applications", ["huayra-about.desktop"]),
                      ("share/icons/hicolor/scalable/apps",["src/media/huayra-about.svg"]),
                      ("share/man/man1", ["huayra-about.1"]) ] )
