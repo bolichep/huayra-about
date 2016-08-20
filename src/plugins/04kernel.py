014a3344b2f5555b6a0421b895f981bc9e50c201
bolichep <bolichep@gmail.com>
2016-08-19 22:54:42 -0300

    cambio comando uname por llamada os.uname()

--- src/plugins/04kernel.py
+++ src/plugins/04kernel.py
@@ -3,20 +3,19 @@
 # License: GPLv3 (see http://www.gnu.org/licenses/gpl.html)
 import markup
 import info_table
+import os
 
 from subprocess import check_output
 
 
 def kernel():
-    running_kernel = check_output(['uname', '-r', '-v']).split()
-    krel_label = markup.label_set_markup('Kernel lanzamiento')
-    krel_text = markup.text_set_markup(running_kernel[0])
+    running_kernel = os.uname()
     kver_label = markup.label_set_markup('Kernel versión')
-    kver_text = markup.text_set_markup(running_kernel[3] + ' ' + running_kernel[4])
+    kver_text = markup.text_set_markup(" ".join(running_kernel[3].split()[2:4]))
 
-    return krel_label, krel_text, kver_label, kver_text
+    return kver_label, kver_text
 
 
-info_table.add_row_to_table(kernel()[2], kernel()[3], 5, "Versión de compilación del kernel")
+info_table.add_row_to_table(kernel()[0], kernel()[1], 5, "Versión de compilación del kernel")
 
 #rint __name__
